8d20ae0ffa82aff165aee413fb9981bf371d788e
CristianDaniel1 <cristiandaniel.1050@gmail.com>
2025-06-07 19:31:07 -0300

    feat: nova função utilitária para formatação de datas

--- client/src/utils/formatting.ts
+++ client/src/utils/formatting.ts
@@ -2,3 +2,9 @@ export const currencyFormatter = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
 });
+
+export const dateFormatter = (date: string | Date) =>
+  new Date(date).toLocaleString();
+
+export const partialDateFormatter = (date: string | Date) =>
+  new Date(date).toLocaleDateString();
